befba76ed2b364390cefef9a150a5021fc102781
venkinaidu <ulabalavenkinaidu@gmail.com>
2025-05-11 11:48:44 +0530

    epsode

---
+++ App.js
@@ -0,0 +1,27 @@
+const heading=React.createElement("div", {id:"parent"}, 
+    
+
+    React.createElement("div", {id:"child"},[
+        [React.createElement("h1", {key:"h1"}, "i am from the root"),
+            React.createElement("h2", {key:"h2"}, "i am from h2")
+        ]
+
+   ] )  
+    
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(heading)
