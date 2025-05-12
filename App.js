78dc5497d06e5ea049ec28347f237a70dec3bd12
venkinaidu <ulabalavenkinaidu@gmail.com>
2025-05-12 12:46:32 +0530

    added

--- App.js
+++ App.js
@@ -1,24 +1,20 @@
+
+   import React from "react";
+   import ReactDOM from "react-dom/client";
+
+
+
 const heading=React.createElement("div", {id:"parent"}, 
     
 
     React.createElement("div", {id:"child"},[
-        [React.createElement("h1", {key:"h1"}, "i am from the root"),
+        [React.createElement("h1", {key:"h1"}, "i am from the root🚀 "),
             React.createElement("h2", {key:"h2"}, "i am from h2")
         ]
 
    ] )  
     
 );
-
-
-
-
-
-
-
-
-
-
 
 
 
