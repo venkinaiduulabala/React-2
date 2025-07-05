0418b8b0cd8461a5132dde10511693648d842b78
venkinaidu <ulabalavenkinaidu@gmail.com>
2025-07-05 13:28:32 +0530

    "change"

--- src/utils/Cart.js
+++ src/utils/Cart.js
@@ -1,12 +1,32 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import ItemLIst from "../components/ItemList"
+//import { clearCart } from "./cartSlice"
+import { useDispatch } from "react-redux";
+import { clearCart } from "../utils/cartSlice";
+import { removeItem } from "../utils/cartSlice";
+
 
 const Cart = () => {
     
   const cartItems = useSelector((store) => store.cart.items)
+
+  const dispatch = useDispatch();
+
+   const onClear = () => {
+     dispatch(clearCart());
+   };
+
+   const onRemove = () => {
+    dispatch(removeItem())
+   }
+
     return (
       <div className="text-center m-2 p-2 text-2xl ">
        <h1 className=" font-bold">Cart</h1>
+      <div className="flex justify-center">
+       <h1 className="p-2 m-2 bg-black border-2 text-white rounded-2xl " onClick={onClear}>Clear cart</h1>
+       <h1 className="p-2 m-2  bg-black border-2 text-white rounded-2xl" onClick={onRemove}>Remove Item</h1>
+       </div>
         <div className="w-8/12 m-auto ">
           <ItemLIst  items={cartItems}/>
         </div>
